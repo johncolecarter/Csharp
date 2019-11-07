7ce5319c92a1d4a84e45608cdeaa85298b54d856
johncolecarter <colecarter7777@gmail.com>
2019-11-07 09:56:08 -0600

    towersOfHanoi

--- dayOneProject/towersOfHanoi/towersOfHanoi.cs
+++ dayOneProject/towersOfHanoi/towersOfHanoi.cs
@@ -6,9 +6,15 @@ namespace towersOfHanoi
 {
     class Program
     {
+        private static Dictionary<String, Stack<int>> Board = new Dictionary<string, Stack<int>>();
+        private static Stack<int> A = new Stack<int>();
+        private static Stack<int> B = new Stack<int>();
+        private static Stack<int> C = new Stack<int>();
+
         static void Main(string[] args)
         {
             game();
+            CheckWin();
         }
         public static void game()
         {
@@ -17,23 +23,18 @@ namespace towersOfHanoi
             Console.WriteLine("B: ");
             Console.WriteLine("C: 1234");
             Console.WriteLine("\n");
-            Dictionary<String, Stack<int>> Board = new Dictionary<string, Stack<int>>();
-            int int1 = 1;
-            int int2 = 2;
-            int int3 = 3;
-            int int4 = 4;
-
-            Stack<int> A = new Stack<int>();
-            Stack<int> B = new Stack<int>();
-            Stack<int> C = new Stack<int>();
-            A.Push(int4);
-            A.Push(int3);
-            A.Push(int2);
-            A.Push(int1);
+            
+            A.Push(4);
+            A.Push(3);
+            A.Push(2);
+            A.Push(1);
      
             Board.Add("A: ", A);
             Board.Add("B: ", B);
             Board.Add("C: ", C);
+            
+            int number = 0;
+            int number2 = 0;
 
             foreach (var item in Board)
             {
@@ -44,10 +45,10 @@ namespace towersOfHanoi
                     Console.Write(array[i]);
                 }
                 Console.WriteLine();
+
             }
-            int playerScore = 0;
-            int number = 0;
-            while (playerScore == 0)
+
+            while (C.Count != 4)
             {
                 Console.WriteLine("Choose the stack you want to move from");
                 string userInput = Console.ReadLine();
@@ -56,13 +57,43 @@ namespace towersOfHanoi
                 if (userInput == "a" || userInput == "A")
                 {
                     number = A.Pop();
+                    
                     if (userInput2 == "b" || userInput2 == "B")
                     {
-                        B.Push(number);
+                        if (B.Count == 0)
+                        {
+                            B.Push(number);
+                        } else if(B.Count > 0)
+                        {
+                           number2 = B.Peek();
+                             if(number > number2)
+                             {
+                                Console.WriteLine("Illegal move.");
+                                A.Push(number);
+                             } else
+                            {
+                                B.Push(number);
+                            }
+                        }
                     }
                     else if (userInput2 == "c" || userInput == "C")
                     {
-                        C.Push(number);
+                        if (C.Count == 0)
+                        {
+                            C.Push(number);
+                        }
+                        else if (C.Count > 0)
+                        {
+                            number2 = C.Peek();
+                            if (number > number2)
+                            {
+                                Console.WriteLine("Illegal move.");
+                                A.Push(number);
+                            } else
+                            {
+                                C.Push(number);
+                            }
+                        }
                     }
                 }
                 if (userInput == "b" || userInput == "B")
@@ -70,11 +101,43 @@ namespace towersOfHanoi
                     number = B.Pop();
                     if (userInput2 == "a" || userInput2 == "A")
                     {
-                        A.Push(number);
+                        if (A.Count == 0)
+                        {
+                            A.Push(number);
+                        }
+                        else if (A.Count > 0)
+                        {
+                            number2 = A.Peek();
+                            if (number > number2)
+                            {
+                                Console.WriteLine("Illegal move.");
+                                B.Push(number);
+                            }
+                            else
+                            {
+                                A.Push(number);
+                            }
+                        }
                     }
                     else if (userInput2 == "c" || userInput == "C")
                     {
-                        C.Push(number);
+                        if (C.Count == 0)
+                        {
+                            C.Push(number);
+                        }
+                        else if (C.Count > 0)
+                        {
+                            number2 = C.Peek();
+                            if (number > number2)
+                            {
+                                Console.WriteLine("Illegal move.");
+                                B.Push(number);
+                            }
+                            else
+                            {
+                                C.Push(number);
+                            }
+                        }
                     }
                 }
                 if (userInput == "c" || userInput == "C")
@@ -82,11 +145,43 @@ namespace towersOfHanoi
                     number = C.Pop();
                     if (userInput2 == "b" || userInput2 == "B")
                     {
-                        B.Push(number);
+                        if (B.Count == 0)
+                        {
+                            B.Push(number);
+                        }
+                        else if (B.Count > 0)
+                        {
+                            number2 = B.Peek();
+                            if (number > number2)
+                            {
+                                Console.WriteLine("Illegal move.");
+                                C.Push(number);
+                            }
+                            else
+                            {
+                                B.Push(number);
+                            }
+                        }
                     }
                     else if (userInput2 == "a" || userInput == "A")
                     {
-                        A.Push(number);
+                        if (A.Count == 0)
+                        {
+                            A.Push(number);
+                        }
+                        else if (A.Count > 0)
+                        {
+                            number2 = A.Peek();
+                            if (number > number2)
+                            {
+                                Console.WriteLine("Illegal move.");
+                                C.Push(number);
+                            }
+                            else
+                            {
+                                A.Push(number);
+                            }
+                        }
                     }
                 }
                 foreach (var item in Board)
@@ -100,6 +195,13 @@ namespace towersOfHanoi
                     Console.WriteLine();
 
                 }
+            }
+        }
+        public static void CheckWin()
+        {
+           if(C.Count == 4)
+            {
+                Console.WriteLine("You Won!!!!!!!!");
             }
         }
     }

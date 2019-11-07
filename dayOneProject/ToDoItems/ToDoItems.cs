f4b807c8b0d0eb92c93a2b6a51c48ac38e2bcf72
johncolecarter <colecarter7777@gmail.com>
2019-11-06 19:37:01 -0600

    ToDoItems

---
+++ dayOneProject/ToDoItems/ToDoItems.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToDoItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDoItems.List();
+        }
+    }
+    class ToDoItems
+    {
+        
+        private static Dictionary<String, String> dict = new Dictionary<String, String>();
+        private static string descrip;
+        private static string duedate;
+
+        public static void List()
+        {
+            bool runList = true;
+
+            while (runList)
+            {
+                Console.WriteLine("What would you like to put on your ToDo list? Enter \"q\" if you are done.");
+                descrip = Console.ReadLine();
+
+                if(descrip != "q")
+                {
+                    Console.WriteLine("Enter the date (ex. 11-3-19) you need to do it by and it's priority (high, medium, or low).");
+                    duedate = Console.ReadLine();
+                    dict.Add(descrip, duedate);
+                } else if(descrip == "q")
+                {
+                    break;
+                }
+            }
+
+            string[] array = duedate.Split(" ");
+            foreach(var key in dict.Keys)
+            {
+                Console.WriteLine($"ToDo item: {key} | Due Date: {array[0]} | Priority: {array[1]}");
+            }
+        }
+    }
+}
--- dayOneProject/towersOfHanoi/towersOfHanoi.cs
+++ dayOneProject/towersOfHanoi/towersOfHanoi.cs
@@ -12,20 +12,95 @@ namespace towersOfHanoi
         }
         public static void game()
         {
+            Console.WriteLine("Welcome to Towers Of Hanoi! Your board should look like this at the end.");
+            Console.WriteLine("A: ");
+            Console.WriteLine("B: ");
+            Console.WriteLine("C: 1234");
+            Console.WriteLine("\n");
             Dictionary<String, Stack<int>> Board = new Dictionary<string, Stack<int>>();
-            Stack A = new Stack();
-            Stack B = new Stack();
-            Stack C = new Stack();
-            Board.Add("A: ", A, 1, 2, 3, 4);
-            Board.Add("B: ", B, null);
+            int int1 = 1;
+            int int2 = 2;
+            int int3 = 3;
+            int int4 = 4;
 
+            Stack<int> A = new Stack<int>();
+            Stack<int> B = new Stack<int>();
+            Stack<int> C = new Stack<int>();
+            A.Push(int4);
+            A.Push(int3);
+            A.Push(int2);
+            A.Push(int1);
+     
+            Board.Add("A: ", A);
+            Board.Add("B: ", B);
+            Board.Add("C: ", C);
 
-            
-            printBoard();
-        }
-        public static void printBoard()
-        {
-            
+            foreach (var item in Board)
+            {
+                Console.Write(item.Key);
+                var array = item.Value.ToArray();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]);
+                }
+                Console.WriteLine();
+            }
+            int playerScore = 0;
+            int number = 0;
+            while (playerScore == 0)
+            {
+                Console.WriteLine("Choose the stack you want to move from");
+                string userInput = Console.ReadLine();
+                Console.WriteLine("Choose which one you want to move to.");
+                string userInput2 = Console.ReadLine();
+                if (userInput == "a" || userInput == "A")
+                {
+                    number = A.Pop();
+                    if (userInput2 == "b" || userInput2 == "B")
+                    {
+                        B.Push(number);
+                    }
+                    else if (userInput2 == "c" || userInput == "C")
+                    {
+                        C.Push(number);
+                    }
+                }
+                if (userInput == "b" || userInput == "B")
+                {
+                    number = B.Pop();
+                    if (userInput2 == "a" || userInput2 == "A")
+                    {
+                        A.Push(number);
+                    }
+                    else if (userInput2 == "c" || userInput == "C")
+                    {
+                        C.Push(number);
+                    }
+                }
+                if (userInput == "c" || userInput == "C")
+                {
+                    number = C.Pop();
+                    if (userInput2 == "b" || userInput2 == "B")
+                    {
+                        B.Push(number);
+                    }
+                    else if (userInput2 == "a" || userInput == "A")
+                    {
+                        A.Push(number);
+                    }
+                }
+                foreach (var item in Board)
+                {
+                    Console.Write(item.Key);
+                    var array = item.Value.ToArray();
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        Console.Write(array[i]);
+                    }
+                    Console.WriteLine();
+
+                }
+            }
         }
     }
 }
